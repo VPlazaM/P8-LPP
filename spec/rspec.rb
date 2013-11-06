cee553725b95254702b1837c8ea5a22bb9b672b4
EduBP <edu_bp89@hotmail.com>
2013-11-06 09:25:06 +0000

    Creado test suma y resta de matrices

--- spec/rspec.rb
+++ spec/rspec.rb
@@ -6,8 +6,8 @@ describe Matriz do
   before :each do
     temp = File.open('./lib/matrices.dat').read
     matriz1, matriz2 = temp.split(/\n\n+/)
-    @Matriz1 = Matriz.new(matriz1)
-    @Matriz2 = Matriz.new(matriz2)
+    @Matriz1 = Matriz.new(matriz1) #[[1, 2], [3, 4]]
+    @Matriz2 = Matriz.new(matriz2) #[[2, 3], [4, 5]]
   end
 
     describe "Visualización de la matriz" do
@@ -21,6 +21,18 @@ describe Matriz do
       it "- La matriz se genera correctamente" do
         temp = Matriz.new("1 2\n3 4")
         @Matriz1.imp.should eq(temp.imp)
+      end
+    end
+    
+    describe "Operaciones con matrices" do
+      it "- La suma matricial se realiza correctamente" do
+	temp = [[3,5],[7,9]]
+        (@Matriz1 + @Matriz2).imp.should eq(temp.inspect)
+      end
+      
+       it "- La suma matricial se realiza correctamente" do
+	temp = [[1,1],[1,1]]
+        (@Matriz2 + @Matriz1).imp.should eq(temp.inspect)
       end
     end
 end
